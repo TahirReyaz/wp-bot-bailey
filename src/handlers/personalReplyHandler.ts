7a42e0cdd861cf79e0574af3c1bad7dd15392989
TahirReyaz <tahir200213@st.jmi.ac.in>
2023-01-25 20:44:30 +0530

    Added remove default reply

--- src/handlers/personalReplyHandler.ts
+++ src/handlers/personalReplyHandler.ts
@@ -1,5 +1,8 @@
 import { proto, WASocket } from "@adiwajshing/baileys";
-import { addDefaultReply } from "../helpers/personalReplyHelpers";
+import {
+  addDefaultReply,
+  removeDefaultReply,
+} from "../helpers/personalReplyHelpers";
 import { queryHandler } from "./queryHandler";
 
 export const personalReplyHandler = async (
@@ -8,10 +11,13 @@ export const personalReplyHandler = async (
   command: string,
   chatId: string
 ) => {
-  const { queryArray, queryPart } = queryHandler(command);
+  const { query, queryArray, queryPart } = queryHandler(command);
   switch (queryArray[0]) {
     case "adr":
       addDefaultReply(sock, message, chatId, queryPart[0], queryPart[1]);
+      break;
+    case "rdr":
+      removeDefaultReply(sock, message, chatId, query);
       break;
   }
 };
--- src/helpers/personalReplyHelpers.ts
+++ src/helpers/personalReplyHelpers.ts
@@ -1,6 +1,6 @@
 import { proto, WASocket } from "@adiwajshing/baileys";
 import axios, { AxiosResponse } from "axios";
-import { addReply, personalData } from "../../data/personalData";
+import { addReply, personalData, removeReply } from "../../data/personalData";
 
 export const addDefaultReply = async (
   sock: WASocket,
@@ -21,6 +21,48 @@ export const addDefaultReply = async (
     await sock.sendMessage(
       chatId,
       { text: `Added ${id} with default reply of\n"${reply}"` },
+      { quoted: message }
+    );
+  } catch (firebaseErr) {
+    console.error({ firebaseErr });
+  }
+};
+
+export const removeDefaultReply = async (
+  sock: WASocket,
+  message: proto.IWebMessageInfo,
+  chatId: string,
+  id: string
+) => {
+  console.log({ id });
+  const reply = personalData.replies.find(({ id: replyId }) => {
+    let found: boolean = false;
+    if (id.length >= 5) {
+      found = replyId.includes(id);
+    } else {
+      found = replyId === id;
+    }
+    return found;
+  });
+
+  //   If there is no reply registered for this number, then return
+  if (!reply) {
+    await sock.sendMessage(
+      chatId,
+      { text: "There is not default reply for this number" },
+      { quoted: message }
+    );
+    return;
+  }
+
+  try {
+    await axios.delete(
+      `${process.env.FIREBASE_DOMAIN}/personal/replies/${reply.firebaseId}.json`
+    );
+    removeReply(reply.firebaseId);
+    await sock.sendMessage(
+      chatId,
+      { text: "Deleted default reply for this number" },
       { quoted: message }
     );
   } catch (firebaseErr) {
